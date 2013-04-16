af08f3c3e53cc5ac677650452c7dfe0b39c5a73e
Albert Cervera i Areny <albert@nan-tic.com>
2013-04-17 00:35:57 +0200

    Add URL to test-flakes.sh.

--- test-flakes.sh
+++ test-flakes.sh
@@ -12,15 +12,17 @@ echo "<body>"
 echo "<table><tr><th>Module</th><th>Output</th></tr>"
 for dir in `ls -1 $main`; do
     full=$main$dir
-    output=$(pyflakes $full | grep -v __init__.py:)
+    output=$(pyflakes $full 2>/dev/null | grep -v __init__.py:)
     if [[ -z "$output" ]]; then
         color="green"
     else
         color="red"
     fi
+    url=$(cat $full/.hg/hgrc 2>/dev/null | grep -v paths | cut -d ' ' -f 3)
     echo "<tr>"
     echo "<td style='border-bottom-style: solid; border-bottom-width: 1px; background-color: $color'>$dir</td>"
     echo "<td style='border-bottom-style: solid; border-bottom-width: 1px; background-color: $color'><pre>$output</pre></td>"
+    echo "<td style='border-bottom-style: solid; border-bottom-width: 1px; background-color: $color'>$url</td>"
     echo "</tr>"
 done
 echo "</table>"
