b3bfaa6f8d332bf0d48a923874b3edb4b67cf49a
Albert Cervera i Areny <albert@nan-tic.com>
2013-07-12 00:40:37 +0200

    Remove test postgresql databases after running the tests.

--- super-runner.sh
+++ super-runner.sh
@@ -9,3 +9,6 @@ key=$1
 echo "${*:2}"
 
 ssh-agent /bin/bash -c "ssh-add $key && ./runtests.py $*"
+
+# Remove postgresql test databases
+psql -l | grep test_ | cut -d " " -f 2 | awk '{system("dropdb "$1)}'
